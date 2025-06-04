8d72232be3be6192d69e202c708c9cc3ead6f201
MOHAMED THANVEER <thanveerami612@gmail.com>
2025-06-04 23:24:01 +0530

    fetch and display user's public IP address using ipify API on home page

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+import requests
+
 # Create your views here.
 def index(request):
-    return render(request, "frontend/home.html")
+    try:
+        response = requests.get('https://api.ipify.org/?format=json')
+        if response.status_code == 200:
+            data = response.json()  # {'ip': 'x.x.x.x'}
+            return render(request, 'frontend/home.html', data)
+        else:
+            return render(request, 'frontend/home.html', {'error': 'Failed to fetch IP'})
+    except requests.exceptions.RequestException as e:
+        return render(request, 'frontend/home.html', {'error': str(e)})
