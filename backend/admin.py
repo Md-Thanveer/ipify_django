16d16c7add627cf432db5c20135f06802ecc7399
MOHAMED THANVEER <thanveerami612@gmail.com>
2025-06-04 23:26:49 +0530

    Update admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from backend.models import Category
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('name','public_ip')
+
+    exclude = ('public_ip',)  # Hides it from the form
